9b7d47b7c481b7ca7d35ad9308c571fdc08fc992
Blinki4 <dmitriy.velg@mail.ru>
2025-10-23 11:19:30 +0300

    CRUD completed, some refactoring

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 from fastapi import FastAPI, Depends, status, Response
 
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
-from sqlalchemy import select
-from pydantic import BaseModel
-from typing import Annotated, Union
+from sqlalchemy import select, delete
+from typing import Annotated
 
 from router import Router
 from models.game_model import GameModel, Base
-
+from shemes.game_scheme import GameUpdateSchema, GameCreateShema
 
 app = FastAPI()
 
@@ -27,44 +26,11 @@ async def get_session():
 SessionDep = Annotated[AsyncSession, Depends(get_session)]
 
 
-##################
-
-class GameCreateShema(BaseModel):
-    title: str
-    description: Union[str, None] = None
-    rating: Union[int, None] = None
-    image_path: Union[str, None] = None
-
-
-class GameSchema(GameCreateShema):
-    id: int
-
-
-class GameUpdateSchema(BaseModel):
-    title: Union[str, None] = None
-    description: Union[str, None] = None
-    rating: Union[int, None] = None
-    image_path: Union[str, None] = None
-
-############
-
-@app.post(
-    Router.SETUP_DB,
-    summary='Не юзать',
-    tags=['Private'],
-)
-async def setup_database():
-    async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.drop_all)
-        await conn.run_sync(Base.metadata.create_all)
-    return {'message': 'Success'}
-
-
 @app.post(
         Router.CREATE_GAME,
+        status_code=status.HTTP_201_CREATED,
         summary='Добавить игру', 
         tags=['Games'],
-        status_code=status.HTTP_201_CREATED,
         )
 async def create_game(data: GameCreateShema, session: SessionDep, response: Response):
     new_game = GameModel(
@@ -79,7 +45,6 @@ async def create_game(data: GameCreateShema, session: SessionDep, response: Resp
             'game': {
                 'id': new_game.id,
                 'title': new_game.title,
-                'description': new_game.description,
                 'rating': new_game.rating,
                 'image_path': new_game.image_path,
     }}
@@ -112,16 +77,56 @@ async def update_game(game_id: int, data: GameUpdateSchema, session: SessionDep,
         response.status_code = status.HTTP_404_NOT_FOUND
         return {'message': 'Not Found'}
     
-    #TODO if title -> data.title ...
+
     if data.title:
         game.title = data.title
     if data.description:
         game.description = data.description
     if data.rating:
         game.rating = data.rating
-    if data.image_path:
+    if data.image_path or data.image_path == '':
         game.image_path = data.image_path
 
     await session.commit()
     await session.refresh(game)
     return game
+
+
+@app.get(
+        '/games/{game_id}',
+        summary='Получить игру по id',
+        tags=['Games']
+        )
+async def get_game(game_id: int, session: SessionDep, response: Response):
+    query = select(GameModel).where(GameModel.id == game_id)
+    result = await session.execute(query)
+    game = result.scalar_one_or_none()
+    if not game:
+        response.status_code = status.HTTP_404_NOT_FOUND
+        return {'message': 'Not Found'}
+    return game
+
+
+@app.delete(
+    '/games/{game_id}',
+    summary='Удалить игру по id',
+    tags=['Games']
+)
+async def delete_game(game_id: int, session: SessionDep, response: Response):
+    query = delete(GameModel).where(GameModel.id == game_id)
+    await session.execute(query)
+    await session.commit()
+    return {'message': 'Success'}
+
+
+
+@app.post(
+    Router.SETUP_DB,
+    summary='Не юзать',
+    tags=['Private'],
+)
+async def setup_database():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
+        await conn.run_sync(Base.metadata.create_all)
+    return {'message': 'Success'}
---
+++ shemes/game_scheme.py
@@ -0,0 +1,19 @@
+from typing import Union
+from pydantic import BaseModel
+
+
+class GameUpdateSchema(BaseModel):
+    title: Union[str, None] = None
+    description: Union[str, None] = None
+    rating: Union[int, None] = None
+    image_path: Union[str, None] = None
+
+class GameCreateShema(GameUpdateSchema):
+    title: str
+    description: Union[str, None] = None
+    rating: Union[int, None] = None
+    image_path: Union[str, None] = None
+
+
+class GameSchema(GameCreateShema):
+    id: int
