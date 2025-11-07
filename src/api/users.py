0b00d1eedcc8cfefdd09f689f3fbbf01d9c8606a
Blinki4 <dmitriy.velg@mail.ru>
2025-11-07 21:29:35 +0300

    authorization complete

--- src/api/users.py
+++ src/api/users.py
@@ -4,6 +4,7 @@ from sqlalchemy import select
 from src.shemas.users import UserRegisterSchema, UserLoginSchema
 from src.models.users import UsersModel
 from src.api.dependencies import SessionDep
+from src.api.config import security, config
 
 
 router = APIRouter()
@@ -15,7 +16,6 @@ router = APIRouter()
         tags=['Users 😐']
         )
 async def register(creds: UserRegisterSchema, session: SessionDep):
-    # Проверить нет ли уже такого пользователя в бд
     query = select(UsersModel).where(UsersModel.username == creds.username)
     result = await session.execute(query)
     user = result.scalar_one_or_none()
@@ -40,4 +40,15 @@ async def register(creds: UserRegisterSchema, session: SessionDep):
         tags=['Users 😐']
         )
 async def login(creds: UserLoginSchema, session: SessionDep, response: Response):
-    ...
+    query = select(UsersModel).where(UsersModel.username == creds.username)
+    result = await session.execute(query)
+    user = result.scalar_one_or_none()
+    if not user:
+        raise HTTPException(404, 'Пользователь не найден')
+    if user.password != creds.password:
+        raise HTTPException(403, 'Неправильный логин или пароль')
+    if user.password == creds.password:
+        token = security.create_access_token(uid = str(user.id))
+        response.set_cookie(config.JWT_ACCESS_COOKIE_NAME, token)
+        response.set_cookie('uid', user.id)
+    return {'message': 'Success'}
--- src/database.py
+++ src/database.py
@@ -2,9 +2,11 @@ from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, Asyn
 from sqlalchemy.orm import DeclarativeBase
 
 
-engine = create_async_engine(
-    "postgresql+asyncpg://admin:SS4MQ9FEEwEUlrkaU4wkftvKSMuDwVtA@dpg-d3rr6nngi27c73faddrg-a.frankfurt-postgres.render.com/lib_hub",
-    )
+# engine = create_async_engine(
+#     "postgresql+asyncpg://admin:SS4MQ9FEEwEUlrkaU4wkftvKSMuDwVtA@dpg-d3rr6nngi27c73faddrg-a.frankfurt-postgres.render.com/lib_hub",
+#     )
+
+engine = create_async_engine('sqlite+aiosqlite:///lib_hub.db')
 
 
 new_session = async_sessionmaker(engine, expire_on_commit=False)
